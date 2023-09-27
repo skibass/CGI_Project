1f771cb5e517742dcd4fc4d257fee1beb85733f5
WaldoGames <bas.zwegers@gmail.com>
2023-09-27 11:23:59 +0200

    DAL-Genrated

---
+++ CGI_Project/CGI_DAL/Database_Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CGI_DAL.Database_Models;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    public virtual ICollection<RoleRight> RoleRights { get; set; } = new List<RoleRight>();
+}
