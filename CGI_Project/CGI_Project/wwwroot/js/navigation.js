0d81afa04fd3782f5ec4869dec5f405eb3663987
Denzel Huijbers <huijbers15@outlook.com>
2023-10-02 11:13:14 +0200

    language select

---
+++ CGI_Project/CGI_Project/wwwroot/js/navigation.js
@@ -0,0 +1,16 @@
+﻿
+function changeLanguage(langCode, flagImageSrc) {
+    document.querySelector('.lang-control-button').textContent = langCode;
+    document.querySelector('#current-language').src = flagImageSrc;
+}
+
+document.querySelectorAll('.lang-dropdown a.dropdown-item').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault(); 
+
+        var langCode = item.textContent;
+        var flagImageSrc = item.previousElementSibling.src;
+
+        changeLanguage(langCode, flagImageSrc);
+    });
+});
