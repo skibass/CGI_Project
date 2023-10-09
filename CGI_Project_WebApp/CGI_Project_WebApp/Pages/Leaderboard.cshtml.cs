8a22916ab3a69422955387e1544f11f4d84ee6a5
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-10-09 14:04:01 +0200

    UI Leaderboard

---
+++ CGI_Project_WebApp/CGI_Project_WebApp/Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CGI_Project_WebApp.Pages
+{
+    public class LeaderboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
