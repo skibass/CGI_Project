ab43cb710e00fbe7f192be62b8ce5daae8e1ee11
WaldoGames <bas.zwegers@gmail.com>
2023-10-10 11:38:22 +0200

    ?

---
+++ CGI_Project_WebApp/CGI_Project_WebApp_Models/SuggestionList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGI_Project_WebApp_Models
+{
+    public class SuggestionList
+    {
+
+        public List<Suggestion> suggestions { get; set; } = new List<Suggestion>();
+    }
+}
