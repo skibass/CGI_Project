c57d741f6f9f93144ada5f6f609a985f8d5692e8
Denzel Huijbers <huijbers15@outlook.com>
2023-10-26 10:48:02 +0200

    Poll Service, Get recent excursions with most votes

---
+++ CGI_Project_WebApp/CGI_Project_WebApp_Core/Services/PollService.cs
@@ -0,0 +1,35 @@
+﻿using CGI_Project_WebApp_DAL.repositories;
+using CGI_Project_WebApp_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGI_Project_WebApp_Core.Services
+{
+    public class PollService
+    {
+        private readonly PollRepository _pollRepository = new();
+
+        public List<Suggestion> GetRecentExcursions()
+        {
+            List<Poll> polls = _pollRepository.GetPastPolls();
+            List<Suggestion> suggestions = new();
+
+            foreach (var poll in polls)
+            {
+            
+                var mostVotedSuggestion = poll.PollSuggestions
+                    .Select(ps => ps.Suggestion)
+                    .OrderByDescending(s => s.Votes.Count)
+                    .FirstOrDefault();
+
+                suggestions.Add(mostVotedSuggestion);
+            }
+
+            return suggestions;
+        }
+    }
+
+}
