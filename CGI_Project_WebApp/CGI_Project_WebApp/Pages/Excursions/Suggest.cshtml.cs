6c4d98ea2bf044e35d8bce8f0eea27f1840459e0
SamRoden22 <sam.rodenburg@graaggoedonline.nl>
2023-10-30 14:20:35 +0100

    Create suggestion almost done

--- CGI_Project_WebApp/CGI_Project_WebApp/Pages/Excursions/Suggest.cshtml.cs
+++ CGI_Project_WebApp/CGI_Project_WebApp/Pages/Excursions/Suggest.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CGI_Project_WebApp_Core.classes;
+using CGI_Project_WebApp_Models;
 
 namespace CGI_Project_WebApp.Pages.Excursions
 {
     public class SuggestModel : PageModel
     {
+        public SuggestionService SuggestionService = new();
+        [BindProperty]
+        public required Suggestion Suggestion { get; set; }
+        [BindProperty]
+		public int EmployeeId { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+	        if (!ModelState.IsValid)
+	        {
+		        TempData["fail"] = "Could not add suggestion";
+	        }
+
+            if (SuggestionService.TryAddSuggestion(Suggestion.Name, Suggestion.Description, Suggestion.Location,
+                    Suggestion.Exception, EmployeeId))
+            {
+                TempData["success"] = "Suggestion has been added";
+                RedirectToPage();
+            }
+            else
+            {
+                TempData["fail"] = "Could not add suggestion";
+            }
         }
     }
 }
