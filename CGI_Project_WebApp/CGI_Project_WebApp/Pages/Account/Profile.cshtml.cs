a5a3610e42e22e3677bf6987300a67e8a5e894ec
drm0011 <119403263+drm0011@users.noreply.github.com>
2023-11-20 10:07:24 +0100

    format name profile

--- CGI_Project_WebApp/CGI_Project_WebApp/Pages/Account/Profile.cshtml.cs
+++ CGI_Project_WebApp/CGI_Project_WebApp/Pages/Account/Profile.cshtml.cs
@@ -14,11 +14,33 @@ public class ProfileModel : PageModel
     public string UserName { get; set; }
     public string UserEmailAddress { get; set; }
     public string UserProfileImage { get; set; }
+
+    private string FormatName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return string.Empty;
+        }
+
+        var parts = name.Split(' ');
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i].Length > 0)
+            {
+                parts[i] = parts[i].Substring(0, 1).ToUpper() + parts[i].Substring(1).ToLower();
+            }
+        }
+
+        return string.Join(" ", parts);
+    }
+
     public void OnGet()
     {
         UserName = User.Identity.Name;
         UserEmailAddress = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
         UserProfileImage = User.FindFirst(c => c.Type == "picture")?.Value;
+
+        UserName=FormatName(UserName);
 
         if (UserEmailAddress != null)
         {
