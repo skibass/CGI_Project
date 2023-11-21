eaed135090f4b7949701227355d0857451eb4f43
Pim <pim.kuijpers@gmail.com>
2023-11-21 09:50:49 +0100

    Poll page and some other stuff.

--- CGI_Project_WebApp/CGI_Project_WebApp/Pages/Excursions/Poll.cshtml.cs
+++ CGI_Project_WebApp/CGI_Project_WebApp/Pages/Excursions/Poll.cshtml.cs
@@ -9,11 +9,47 @@ namespace CGI_Project_WebApp.Pages.Excursions
 {
     public class PollModel : PageModel
     {
-        [BindProperty]
+		public PollService PollService = new();
+        
+		[BindProperty]
 		public required Poll Poll { get; set; }
+
+		private SuggestionList unusedSuggestionsList;
+
+		public SuggestionList UnusedSuggestionsList
+		{
+			get { return unusedSuggestionsList; }
+			set { unusedSuggestionsList = value; }
+		}
+
+		public EmployeeService EmployeeService = new EmployeeService();
+		
+		public SuggestionService SuggestionsService = new SuggestionService();
 
 		public void OnGet()
         {
-        }
-    }
+			SuggestionsService.TryGetSuggestions(out unusedSuggestionsList);
+		}
+
+		public void OnPost()
+		{
+			Poll pollToSave = new Poll();
+			pollToSave.Poll_name = Poll.Poll_name;
+			
+			//TODO: Finish Error
+			if (!ModelState.IsValid)
+			{
+				
+			}
+
+			if (PollService.TryAddPoll(pollToSave))
+			{
+				RedirectToPage();
+			}
+			else
+			{
+					
+			}
+	    }
+	}
 }
