dde396d143611e35ae4875d813d1d2daac1d0e31
thijn <508700@student.fontys.nl>
2023-12-05 11:16:35 +0100

    Poll pagina toegevoegd en validatie voor manager om poll pagina te kunnen bekijken

---
+++ CGI_Project_WebApp/CGI_Project_WebApp/Pages/Excursions/Poll.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CGI_Project_WebApp.Pages.Excursions
+{
+    public class PollModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            if (User.Identity.IsAuthenticated == false)
+            {
+                return RedirectToPage("../Index");
+            }
+            return null;
+        }
+    }
+}
