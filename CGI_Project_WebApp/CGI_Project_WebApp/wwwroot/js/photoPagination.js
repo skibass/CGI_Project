fc1b5734be06b89b6baca9e8be672f47088650d3
drm0011 <119403263+drm0011@users.noreply.github.com>
2024-01-09 13:21:00 +0100

    responsive gallery

---
+++ CGI_Project_WebApp/CGI_Project_WebApp/wwwroot/js/photoPagination.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var currentPage = 1;
+    var itemsPerPage = 3; 
+
+    function showPage(pageNumber) {
+        var items = document.querySelectorAll("#photoContainer > .photo-item");
+        var totalPages = Math.ceil(items.length / itemsPerPage);
+
+        items.forEach(item => item.style.display = 'none');
+        for (var i = (pageNumber - 1) * itemsPerPage; i < pageNumber * itemsPerPage && i < items.length; i++) {
+            items[i].style.display = 'block';
+        }
+
+        var pagination = document.querySelector(".pagination");
+        pagination.innerHTML = '';
+        for (var i = 1; i <= totalPages; i++) {
+            var pageLink = document.createElement("a");
+            pageLink.innerHTML = i;
+            pageLink.href = "#";
+            pageLink.addEventListener("click", function (event) {
+                event.preventDefault();
+                showPage(parseInt(this.innerHTML));
+            });
+            pagination.appendChild(pageLink);
+        }
+    }
+
+    showPage(currentPage);
+});
